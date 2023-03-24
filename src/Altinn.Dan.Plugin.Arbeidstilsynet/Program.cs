a4117969ddcd6c6c40b963ac9cae18f5b8f70e0e
Bjørn Dybvik Langfors <bdl@digdir.no>
2023-03-24 11:42:57 +0100

    Update dependencies, use common instead of own utils, cleanups

--- src/Altinn.Dan.Plugin.Arbeidstilsynet/Program.cs
+++ src/Altinn.Dan.Plugin.Arbeidstilsynet/Program.cs
@@ -1,19 +1,16 @@
 using Altinn.Dan.Plugin.Arbeidstilsynet.Config;
-using Dan.Common.Compat;
 using Dan.Common.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 var host = new HostBuilder()
         .ConfigureDanPluginDefaults()
         .ConfigureServices((context, services) =>
         {
-            services.Configure<ApplicationSettings>(context.Configuration);
+            services.Configure<Settings>(context.Configuration);
         })
         .Build();
 await host.RunAsync();
-        
-    
+
+
 
